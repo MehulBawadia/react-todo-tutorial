6a877767434221608b178c4c409b8b2d4a1ce827
Mehul Bawadia <mehulbawadia@gmail.com>
2020-10-22 15:22:53 +0530

    Updates

---
+++ precache-manifest.ef03cc2bcae68e0e8761d687d5475707.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f47dc0d189827b7c0e068af5d13434d2",
+    "url": "/react-todo-tutorial/index.html"
+  },
+  {
+    "revision": "4d4a6e20495d74600ea3",
+    "url": "/react-todo-tutorial/static/css/main.42bb7de3.chunk.css"
+  },
+  {
+    "revision": "0d21e879600e56c476c3",
+    "url": "/react-todo-tutorial/static/js/2.3756ac38.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/react-todo-tutorial/static/js/2.3756ac38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d4a6e20495d74600ea3",
+    "url": "/react-todo-tutorial/static/js/main.02f73bf5.chunk.js"
+  },
+  {
+    "revision": "1baa5d110cbf10d5a675",
+    "url": "/react-todo-tutorial/static/js/runtime-main.c67a668d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-todo-tutorial/static/media/logo.5d5d9eef.svg"
+  }
+]);
